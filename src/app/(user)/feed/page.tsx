7a2eed12a448f677a4086ca57436cd29ae61b654
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-10-20 15:31:33 +0530

    setup husky and eslint

--- src/app/(user)/feed/page.tsx
+++ src/app/(user)/feed/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const Feed = () => {
+  const hello = "something"
   return (
-    <div>
+    <div className='bg-blue-500 p-4 text-white rounded-lg hover:bg-blue-600 shadow-md'>
       feed
     </div>
   )
