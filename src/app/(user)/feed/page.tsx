c02ade75ff8b84d93fadf2d4bbcc69e8c5bd79fd
Akinur Rahman <akinurrahmanofficial@gmail.com>
2024-10-20 16:12:44 +0530

    husky test

--- src/app/(user)/feed/page.tsx
+++ src/app/(user)/feed/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Feed = () => {
   return (
     <div className="rounded-lg bg-blue-500 p-4 text-white shadow-md hover:bg-blue-600">
-      hello world
+      hello world world
     </div>
   );
 };
